8f153de4c4c8c4aeab8b39fbe82b080a32e09454
minhtricute69 <minhtricute69@gmail.com>
2021-01-03 01:54:00 +0700

    New version based on Archiso

--- liveiso/airootfs/root/customize_airootfs.sh
+++ liveiso/airootfs/root/customize_airootfs.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env bash
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
@@ -8,14 +7,12 @@ set -e -u
 # Warning: customize_airootfs.sh is deprecated! Support for it will be removed in a future archiso version.
 
 sed -i 's/#\(vi_VN\.UTF-8\)/\1/' /etc/locale.gen
-
 echo "LANG=vi_VN.UTF-8" > /etc/locale.conf
 echo "LANGUAGE=vi_VN.UTF-8" > /etc/locale.conf
 echo "LC_COLLATE=C" > /etc/locale.conf
 echo "LC_ALL=C" > /etc/locale.conf
 locale-gen
 
-# virtual console
 echo "KEYMAP=us" > /etc/vconsole.conf
 
 # Time and clock
@@ -23,16 +20,13 @@ timedatectl set-timezone Asia/Ho_Chi_Minh
 ln -sf /usr/share/zoneinfo/Asia/Ho_Chi_Minh  /etc/localtime
 hwclock --systohc --utc
 
-sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
+#Add user
 usermod -s /usr/bin/zsh root
 cp -aT /etc/skel/ /root
 chmod 700 /root
 
 useradd -m -p ""  -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/zsh liveuser
 usermod -a  -G "adm,audio,video,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" liveuser
-
-
-
 
 sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
@@ -41,7 +35,6 @@ sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
 sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
-
 
 systemctl enable pacman-init.service choose-mirror.service
 systemctl enable lightdm.service
@@ -60,7 +53,7 @@ chown -R liveuser:users /home/liveuser/.config
 chown -R liveuser:users /home/liveuser
 
 
-
+sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 pacman -Sy
 pacman-key --init
 pacman-key --populate archlinux
