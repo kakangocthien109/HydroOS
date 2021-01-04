8bbeec3077ac63244ac9154db386034ecbc47156
minhtricute69 <minhtricute69@gmail.com>
2021-01-04 22:03:14 +0700

    Ban dùng nhan Linux LTS

--- liveiso/airootfs/root/customize_airootfs.sh
+++ liveiso/airootfs/root/customize_airootfs.sh
@@ -5,14 +5,14 @@
 set -e -u
 
 # Warning: customize_airootfs.sh is deprecated! Support for it will be removed in a future archiso version.
-
+#Set locale
 sed -i 's/#\(vi_VN\.UTF-8\)/\1/' /etc/locale.gen
 echo "LANG=vi_VN.UTF-8" > /etc/locale.conf
 echo "LANGUAGE=vi_VN.UTF-8" > /etc/locale.conf
 echo "LC_COLLATE=C" > /etc/locale.conf
 echo "LC_ALL=C" > /etc/locale.conf
 locale-gen
-
+#Set Keyboard
 echo "KEYMAP=us" > /etc/vconsole.conf
 
 # Time and clock
@@ -27,7 +27,7 @@ chmod 700 /root
 
 useradd -m -p ""  -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/zsh liveuser
 usermod -a  -G "adm,audio,video,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" liveuser
-
+#Set permission
 sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
@@ -35,14 +35,14 @@ sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
 sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
 sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
-
+#Set services
 systemctl enable pacman-init.service choose-mirror.service
 systemctl enable lightdm.service
 systemctl set-default graphical.target
 systemctl enable bluetooth.service
 rm -r /etc/systemd/system/getty@tty1.service.d/autologin.conf
 systemctl enable NetworkManager.service
-
+#Set password
 passwd root
 passwd liveuser
 
@@ -56,5 +56,4 @@ chown -R liveuser:users /home/liveuser
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 pacman -Sy
 pacman-key --init
-pacman-key --populate archlinux
 kodepas setup
